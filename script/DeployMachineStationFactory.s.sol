91cf51120c278168f0d26707d1319017c55aac31
Lopez Gonzalo <gonzalo@smtv.dev>
2025-02-24 15:41:35 -0300

    fix: forge format

--- script/DeployMachineStationFactory.s.sol
+++ script/DeployMachineStationFactory.s.sol
@@ -3,7 +3,8 @@ pragma solidity 0.8.25;
 
 import {Script, console} from "forge-std/Script.sol";
 import {MachineStationFactory} from "../src/machine-station/MachineStationFactory.sol";
-import {MachineStationFactory as MachineStationFactoryTestnet} from "../src/machine-station/agung/MachineStationFactory.sol";
+import {MachineStationFactory as MachineStationFactoryTestnet} from
+    "../src/machine-station/agung/MachineStationFactory.sol";
 
 contract DeployGasStation is Script {
     function run() external returns (address factory) {
